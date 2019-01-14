9a3ae383ebc3675c0a3b3cd28cca9d951cc66491
perlancar (on pc-office) <perlancar@gmail.com>
2019-01-14 18:41:10 +0700

    Implement get_available_versions() and get_release_note()

--- lib/Software/Catalog/SW/bulwark/qt.pm
+++ lib/Software/Catalog/SW/bulwark/qt.pm
@@ -30,6 +30,57 @@ sub get_latest_version {
     );
 }
 
+sub get_available_versions {
+    my ($self, %args) = @_;
+
+    my $res = extract_from_url(
+        url => "https://github.com/bulwark-crypto/Bulwark/releases",
+        re  => qr!/bulwark-crypto/Bulwark/tree/([^"/]+)!,
+        all => 1,
+    );
+    return $res unless $res->[0] == 200;
+    # sort versions from earliest
+    $res->[2] = [ sort { $self->cmp_version($a, $b) } @{$res->[2]}];
+    $res;
+}
+
+sub get_release_note {
+    require Mojo::DOM;
+
+    my ($self, %args) = @_;
+    my $format = $args{format} // 'text';
+
+    # 2.1.1 tree means version 2.1.1.0
+    my $version = $args{version} // do {
+        my $res = get_latest_version(%args);
+        return $res unless $res->[0] == 200;
+        $res->[2];
+    };
+    $version =~ s/\.0\z// if $version =~ /\A\d+\.\d+\.\d+\.0\z/;
+
+    my $url = "https://github.com/bulwark-crypto/Bulwark/releases/tag/$version";
+    my $res = extract_from_url(
+      url => $url,
+      code => sub {
+          my %cargs = @_;
+          my $dom = Mojo::DOM->new($cargs{content});
+          my $html = $dom->at(".markdown-body")->content;
+
+          if ($html) {
+              if ($format eq 'html') {
+                  return [200, "OK", $html];
+              } else {
+                  require HTML::FormatText::Any;
+                  return [200, "OK",
+                          HTML::FormatText::Any::html2text(html => $html)->[2]];
+              }
+          } else {
+              return [543, "Cannot scrape release note text from $url"];
+          }
+      },
+  );
+}
+
 sub canon2native_arch_map {
     return +{
         'linux-x86' => 'linux32',
